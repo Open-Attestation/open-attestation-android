0ca620fc92726ca174fb80f4bc2582d48d011d29
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2023-01-04 15:32:29 +0800

    feat: Add OaRendererActivity to open-attestation-android lib

--- app/src/main/java/com/example/open_attestation_android_example/MainActivity.kt
+++ app/src/main/java/com/example/open_attestation_android_example/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.open_attestation_android_example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
+import com.example.open_attestation_android.OaRendererActivity
 import com.example.open_attestation_android.OpenAttestation
 
 class MainActivity : AppCompatActivity() {
     private lateinit var testVerifyButton: Button
+    private lateinit var testViewDocumentButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val filename = "national-youth-council_obs-moc.oa"
+        val document = application.assets.open(filename).bufferedReader().use{
+            it.readText()
+        }
+
         setContentView(R.layout.activity_main)
         testVerifyButton = findViewById<Button>(R.id.testVerifyButton)
         testVerifyButton.setOnClickListener {
-            val file_name = "national-youth-council_obs-moc.oa"
-            val oadoc = application.assets.open(file_name).bufferedReader().use{
-                it.readText()
-            }
 
             val oa = OpenAttestation()
-            oa.verifyDocument(this, oadoc) { isValid ->
+            oa.verifyDocument(this, document) { isValid ->
                 if (isValid) {
                     val alertDialogBuilder = AlertDialog.Builder(this)
                     alertDialogBuilder.setTitle("Verification successful")
@@ -37,6 +41,14 @@ class MainActivity : AppCompatActivity() {
                     alertDialogBuilder.show()
                 }
             }
+        }
+
+        testViewDocumentButton = findViewById<Button>(R.id.testViewDocumentButton)
+        testViewDocumentButton.setOnClickListener {
+            val intent = Intent(this, OaRendererActivity::class.java)
+            intent.putExtra(OaRendererActivity.OA_DOCUMENT_KEY, document)
+            intent.putExtra(OaRendererActivity.OA_DOCUMENT_FILENAME_KEY, filename)
+            startActivity(intent)
         }
     }
 }
